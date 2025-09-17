af7a72f9e7fa960f367e61bf9eecf969525913fe
Muhammad-Ali539 <aligoess3003@gmail.com>
2025-09-17 21:52:09 +0500

    Add files via upload

---
+++ coin flip.py
@@ -0,0 +1,6 @@
+import random
+num= random.randint(0,10)
+if num>0.5:
+    print('heads')
+else:
+    print('tails')
---
+++ hypotonuse.py
@@ -0,0 +1,4 @@
+a=int(input('shortest side'))
+b=int(input('another short side'))
+c=(a*a+b*b)**1/2
+print(c)
